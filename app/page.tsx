a3797dd5ab9559b975fa876b0d1a81881a4f9958
mohak <c.mohak2004@gmail.com>
2024-09-21 23:10:56 +0530

    added some navigations

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,16 @@
+import { getServerSession } from "next-auth";
+import { redirect } from 'next/navigation'
+import { authOptions } from "./lib/auth";
 
-
-import AppbarClient from "@/components/appbarClient";
-
-
-
-export default function Home() {
-
-  return (
-   <div>
- 
-   </div>
-  );
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+  if (session?.user) {
+    redirect('/dashboard')
+  } else {
+    redirect('/api/auth/signin')
+  }
+  
 }
-
 
 
 
